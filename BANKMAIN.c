5ce623b7696b57b49f78152e256b443477ea2297
sameh.khalil <samehkh35@gmail.com>
2022-09-02 17:13:51 +0200

    firstcommit

---
+++ BANKMAIN.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include "BIT_MATH.h"
+#include "STD_TYPES.h"
+
+int main (int argc , char * argv[])
+{
+	// read data base
+	//system in super loop 
+	while (1)
+	{
+			/* if employee is chosen then he need to enter user name and password */
+			/*search if the username and password is correct in the database we need to handel the data base*/
+			//show a list of options 
+				//option 1 creat new account 
+					//-ask for full name --check its at least first 4 names (else ask him again )
+					//-ask 4 full address 
+					//-ask 4 age 
+					//ask 4 national id check for 14 digit(else ask him again)
+					//-if age < 21 -ask 4 gaurdian national id check for 14 digit(else ask him again)
+					//-ask for balance
+					//send the new account information and default password 
+					//name address nID age bankid accountstatus balance (if age <21)gaurdian nationalid
+				
+				//option 2 open existing account 
+					//showlist of options 
+						//1- transaction --bankid
+						//2-change account status --bankid
+						//3-deposit
+						//return to employee options
+				
+				//option 3 exit
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ STD_TYPES.h
@@ -0,0 +1,19 @@
+
+
+
+#ifndef __STD_TYPES_H__
+#define __STD_TYPES_H__
+
+typedef unsigned char u8 ;
+typedef unsigned short u16 ;
+typedef unsigned long int u32;
+
+
+typedef signed char 	s8 ;
+typedef signed short 	s16 ;
+typedef signed long int s32;
+
+
+
+
+#endif
