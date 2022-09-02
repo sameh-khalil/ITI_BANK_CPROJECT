efd300065fae5f6a2b76c19cc20f8a681ff0bb44
sameh.khalil <samehkh35@gmail.com>
2022-09-02 21:46:50 +0200

    some file manibulation and database setup

--- BANKMAIN.c
+++ BANKMAIN.c
@@ -1,12 +1,62 @@
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "BIT_MATH.h"
 #include "STD_TYPES.h"
 
+
+typedef struct{
+	char ** _n;
+	char ** _p;
+	u32 rowsize ;
+	u32 strsize ;
+}emp_DB_t;
+
+
+
+void parsfilefornamesandpassword (FILE * file, char ** names,char ** password,u32 size);
+
+void reserve_heap_memory(emp_DB_t * db) ;
+
+u32 DBROWSSIZE (FILE * file);
+
 int main (int argc , char * argv[])
 {
-	// read data base
+	//data base setup
+	FILE * emp =fopen("./employeeDB.txt","rw");
+	FILE * empsize =fopen("./embdbsize.txt","rw");
+
+	if(emp==NULL||empsize==NULL)
+	{printf("file didn't open \n");}
+	else
+	{
+		emp_DB_t DB_ ;
+		DB_.rowsize=DBROWSSIZE(empsize);
+		DB_.strsize=30;
+		
+		printf("%d\n",DB_.rowsize);
+		reserve_heap_memory(&DB_);
+		parsfilefornamesandpassword(emp,DB_._n,DB_._p,DB_.rowsize);
+	}
+	
+	fclose(emp);
+	fclose(empsize);
 	//system in super loop 
+
+#if 0
+	else	
+	{
+		int i = 0 ;
+		int j = 0 ;
+		char pars[100][100]  ;
+		while(fscanf(emp,"%c",pars[i])==1)
+		{
+			while()
+		}
+	}
+#endif
+	#if 0
 	while (1)
 	{
 			/* if employee is chosen then he need to enter user name and password */
@@ -35,8 +85,60 @@ int main (int argc , char * argv[])
 		
 	}
 	
-	
+	#endif
 	
 	
 	
 }
+
+
+
+
+u32 DBROWSSIZE (FILE * file)
+{
+	int j = 1  ;
+	u32 size= 0;
+	char ch[20] ;
+	fgets(ch,20,file);
+	for(int i = strlen(ch)-1 ; i >=0 ;i-- )
+	{
+		size += ((int)(ch[i]-'0'))*j;
+		j*=10;
+	}
+	return size ;
+}
+
+
+
+
+void reserve_heap_memory(emp_DB_t * db )
+{
+		db->_n = (char ** )malloc(db->rowsize*sizeof(char*));
+		db->_p = (char ** )malloc(db->rowsize*sizeof(char*));
+
+		for(u32 i = 0 ; i < db->rowsize ; i ++)
+		{
+			db->_n[i]=(char*)malloc(db->strsize*sizeof(char));
+			db->_p[i]=(char*)malloc(db->strsize*sizeof(char));
+
+		}
+		
+}
+
+void parsfilefornamesandpassword (FILE * file, char ** names,char ** password,u32 size)
+{
+	u32 i= 0 , j = 0 ;
+	while (fscanf(file, "%s ", names[i]) == 1 &&fscanf(file, "%s ", password[i]) == 1 )
+    {
+		i++;		
+		if(i==size)
+		{break;}
+	}
+	j=i;
+	for(i = 0 ; i < j ; i++)
+	{printf("%s\n",names[i]);}
+	printf("--------------\n");
+	for(i = 0 ; i < j ; i++)
+	{printf("%s\n",password[i]);}
+}
+
